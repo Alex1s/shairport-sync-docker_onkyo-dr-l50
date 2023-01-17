60a93a231d940795301aa825e500460ce87e1277
user.name <user.email>
2023-01-17 21:40:14 +0100

    only log ir-ctl output if unexpected

--- api/infrared_transmitter.py
+++ api/infrared_transmitter.py
@@ -1,4 +1,6 @@
 import asyncio.subprocess
+import math
+
 from logger import logger
 
 from models import Power
@@ -10,8 +12,7 @@ POWER_OFF_MODE2 = '/mode2/power_off.mode2'
 VOLUME_UP_MODE2 = '/mode2/volume_up.mode2'
 VOLUME_DOWN_MODE2 = '/mode2/volume_down.mode2'
 
-POWER_GAP = 1  # 1 second
-VOLUME_GAP = 70e-3  # 70ms
+GAP = 70e-3  # 70ms
 
 
 def airplay_volume_to_receiver_volume(airplay_volume: float) -> int:
@@ -26,15 +27,13 @@ def airplay_volume_to_receiver_volume(airplay_volume: float) -> int:
 
 async def ir_ctl(mode2_file: str, repeat: int = 0) -> None:
     send_args = [f'--send={mode2_file}'] * (repeat + 1)
-    all_args = [IR_CTL_COMMAND, '--carrier=0'] + send_args
+    all_args = [IR_CTL_COMMAND, '--carrier=0', f'--gap={math.ceil(GAP * 1e6)}'] + send_args
     logger.warning(f'Running ir-ctl: {all_args}')
     subprocess = await asyncio.subprocess.create_subprocess_exec(*all_args)
     result = await subprocess.wait()
-    logger.warning(f'ir-ctl stdout: {subprocess.stdout}')
-    logger.warning(f'ir-ctl stderr: {subprocess.stdout}')
-    logger.warning(f'result: {result}')
-    if result:
-        raise RuntimeError(f'ir-ctl returned non-zero: {result}')
+    assert subprocess.stdout is None, f'ir-ctl stdout: {subprocess.stdout}'
+    assert subprocess.stderr is None, f'ir-ctl stderr: {subprocess.stderr}'
+    assert result == 0, f'ir-ctl result: {result}'
 
 
 async def power(p: Power) -> None:
@@ -46,23 +45,23 @@ async def power(p: Power) -> None:
 
 async def power_on() -> None:
     await ir_ctl(POWER_ON_MODE2)
-    await asyncio.sleep(POWER_GAP)
+    await asyncio.sleep(GAP)
 
 
 async def power_off() -> None:
     await ir_ctl(POWER_OFF_MODE2)
-    await asyncio.sleep(POWER_GAP)
+    await asyncio.sleep(GAP)
 
 
 async def volume_up(num: int = 1) -> None:
     if num == 0:
         return
     await ir_ctl(VOLUME_UP_MODE2, num - 1)
-    await asyncio.sleep(VOLUME_GAP)
+    await asyncio.sleep(GAP)
 
 
 async def volume_down(num: int = 1) -> None:
     if num == 0:
         return
     await ir_ctl(VOLUME_DOWN_MODE2, num - 1)
-    await asyncio.sleep(VOLUME_GAP)
+    await asyncio.sleep(GAP)
